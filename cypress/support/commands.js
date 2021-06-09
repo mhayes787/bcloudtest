1cbe6e3bb290e7d31c5611585fd6bafa8bba9a66
Maurice Hayes <mhayes787@maurices-mbp.attlocal.net>
2021-06-09 14:44:31 -0400

    Setup and connected to dashboard to save videos and results

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//
+//--This is required to save snapshots
+require('@cypress/snapshot').register()
